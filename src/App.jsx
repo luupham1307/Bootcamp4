70f5e8dff0dfc29b60e97323df2071edad5738dd
luupham1307 <hoangluupham1307@gmail.com>
2023-04-07 09:27:20 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import SampleNavbar from "./components/SampleNavbar";
+import SampleComponents3 from "./components/SampleCmponents3";
+import Components3 from "./components/components3";
+
+export default function App() {
+  return (
+    <div>
+      <SampleNavbar />
+      <Navbar />
+      <SampleComponents3 />
+      <Components3 />
+      <hr />
+      {/* <Components3 /> */}
+
+    </div>
+  );
+}
---
+++ src/components/Components3.jsx
@@ -0,0 +1,21 @@
+export default function Components3() {
+    return (
+        <section>
+            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+                <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+                    <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+
+                    </div>
+
+                    <div>
+                        <h2>Grow your audience</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic
+                            atque tenetur quis eius quos ea neque sunt, accusantium soluta minus
+                            veniam tempora deserunt? Molestiae eius quidem quam repellat.</p>
+                        <a href="">Get Started Today</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
